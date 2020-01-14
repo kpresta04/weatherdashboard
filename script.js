718b4291222a1da3c77e7e86a04f806f6eca6e65
Kellen Presta <kpresta04@gmail.com>
2020-01-13 18:43:15 -0600

    changed units to imperial

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
   function todaysWeather(cityName) {
-    const queryURL = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&APPID=0635f6cc93f851c62608a2c527a89527`;
+    const queryURL = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=imperial&APPID=0635f6cc93f851c62608a2c527a89527`;
     $.ajax({
       url: queryURL,
       method: "GET"
     }).then(function(response) {
-      let temp = (response.main.temp - 273.15) * 1.8 + 32;
+      let temp = response.main.temp;
       temp = temp.toFixed(1);
       console.log(response);
       const mainHTML = `
@@ -16,7 +16,7 @@ $(document).ready(function() {
     <h2 class="card-title">${response.name} ${moment().format("l")}</h2>
     <h5>Temperature: ${temp}°F</h5>
       <h5>Humidity: ${response.main.humidity}%</h5>
-      <h5>Wind speed: ${response.wind.speed} m/s</h5>
+      <h5>Wind speed: ${response.wind.speed} MPH</h5>
       <h5>${response.weather[0].main}</h5>
   </div>
 </div>`;
@@ -24,6 +24,16 @@ $(document).ready(function() {
       $("#mainCol").append(mainHTML);
     });
   }
+  function fiveDayForecast(cityName) {
+    const queryURL = `http://api.openweathermap.org/data/2.5/forecast?q=${cityName}&units=imperial&APPID=0635f6cc93f851c62608a2c527a89527`;
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function(response) {
+      // console.log(response);
+    });
+  }
 
   todaysWeather("Overland Park");
+  fiveDayForecast("Overland Park");
 });
