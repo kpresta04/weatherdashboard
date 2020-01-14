ba179a8b4e5db48acba7d40c4bdaf65452c41e89
Kellen Presta <kpresta04@gmail.com>
2020-01-13 20:44:20 -0600

    added 5 days

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
 $(document).ready(function() {
+  const iconCodes = [
+    {
+      id: 200 - 232,
+      descrip: "thunderstorm",
+      dayIcon_url: "http://openweathermap.org/img/wn/11d@2x.png",
+      nightIcon_url: "http://openweathermap.org/img/wn/11n@2x.png"
+    }
+  ];
+
   function todaysWeather(cityName) {
     const queryURL = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=imperial&APPID=0635f6cc93f851c62608a2c527a89527`;
     $.ajax({
       url: queryURL,
       method: "GET"
     }).then(function(response) {
-      let temp = response.main.temp;
-      temp = temp.toFixed(1);
+      const temp = response.main.temp.toFixed(1);
       console.log(response);
-      const mainHTML = `
-      
-      
-      <div class="card" style="width: 18rem;">
+      const mainHTML = `<div class="card" style="width: 18rem;">
+      <img src="http://openweathermap.org/img/wn/11d@2x.png" class="card-img-top" alt="...">
   <div class="card-body">
     <h2 class="card-title">${response.name} ${moment().format("l")}</h2>
     <h5>Temperature: ${temp}°F</h5>
@@ -21,7 +27,7 @@ $(document).ready(function() {
   </div>
 </div>`;
 
-      $("#mainCol").append(mainHTML);
+      $("#mainRow").append(mainHTML);
     });
   }
   function fiveDayForecast(cityName) {
@@ -30,7 +36,26 @@ $(document).ready(function() {
       url: queryURL,
       method: "GET"
     }).then(function(response) {
-      // console.log(response);
+      let addDays = 1;
+      for (let i = 0; i < 40; i += 9) {
+        const temp = response.list[i].main.temp.toFixed(1);
+        // console.log(response);
+        console.log(i);
+        const mainHTML = `<div class="card mx-2" style="width: 18rem;">
+      <img src="http://openweathermap.org/img/wn/11d@2x.png" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h2 class="card-title">${response.city.name} ${moment()
+          .add(addDays, "days")
+          .format("l")}</h2>
+    <h5>Temperature: ${temp}°F</h5>
+      <h5>Humidity: ${response.list[i].humidity}%</h5>
+      <h5>Wind speed: ${response.list[i].wind.speed} MPH</h5>
+      <h5>${response.list[i].weather[0].main}</h5>
+  </div>
+</div>`;
+        addDays++;
+        $("#5dayRow").append(mainHTML);
+      }
     });
   }
 
